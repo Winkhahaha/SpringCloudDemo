3419ddc9a467ece831c2f69fc48c0b52e7eac4c3
高明 <mineok@qq.com>
2020-04-19 00:49:11 +0800

    Hystrix模块构建

--- cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/mineok/springcloud/PaymentHystrixMain8001.java
+++ cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/mineok/springcloud/PaymentHystrixMain8001.java
@@ -5,15 +5,13 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableCircuitBreaker
-// @EnableHystrix
+@EnableHystrix
 public class PaymentHystrixMain8001 {
     public static void main(String args[]) {
         SpringApplication.run(PaymentHystrixMain8001.class, args);
