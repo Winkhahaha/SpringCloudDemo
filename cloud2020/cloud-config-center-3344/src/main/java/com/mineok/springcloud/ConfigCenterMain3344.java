4cc014110762be9ccd7a06f3a740d0ecaac97947
高明 <mineok@qq.com>
2020-04-19 22:13:30 +0800

    配置文件

--- cloud2020/cloud-config-center-3344/src/main/java/com/mineok/springcloud/ConfigCenterMain3344.java
+++ cloud2020/cloud-config-center-3344/src/main/java/com/mineok/springcloud/ConfigCenterMain3344.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-/**
- * @auther zzyy
- * @create 2020-02-21 17:47
- */
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigCenterMain3344
